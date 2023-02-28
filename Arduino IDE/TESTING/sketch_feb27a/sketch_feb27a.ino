b50df9e19352fe93fc6e83c059f1affa92a10a97
Arya Anantula <anantula.arya@gmail.com>
2023-02-27 21:07:57 -0600

    uno 2 way changes

---
+++ Arduino IDE/TESTING/sketch_feb27a/sketch_feb27a.ino
@@ -0,0 +1,123 @@
+/*  Uno Side Code
+* 
+ Module SX1278 // Arduino Uno  
+   Vcc (Red)      ->   3.3V
+   MISO (Brown)   ->   D12
+   MOSI (Green)   ->   D11     
+   SLCK (Yellow)  ->   D13
+   Nss (Orange)   ->   D10
+   REST (Purple)  ->   D9
+   DIO0 (Blue)    ->   D2
+   GND (Black)    ->   GND
+*/
+
+// 2/27/2023 9 PM
+
+#include <SPI.h>
+#include <LoRa.h>  
+
+#define buttonPin 8
+#define ledPin 7
+
+/*
+  Flag to check/switch permissions
+  1 -> switch
+  0 -> reset
+*/
+
+byte flag = 0;  
+byte msgCount = 0;
+byte localAddress = 0xBB;
+byte destination = 0xFF;
+long lastSendTime = 0;
+int interval = 2000;
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("LoRa Init Failed");
+    while (true);
+  }
+
+}
+
+void loop() {
+   while(flag == 0){
+    onReceive(LoRa.parsePacket());    
+   }
+
+   if (digitalRead(buttonPin) == HIGH){
+      sendMessage("ON");
+   }
+   if (digitalRead(buttonPin) == LOW){
+      sendMessage("OFF");
+   }
+   flag = 0;  //Switch permissions
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(flag);                     // add switch flag
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++; // increment message ID
+  Serial.println("Message Sent");
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return "";          // if there's no packet, return
+  
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte switchByte = LoRa.read();        // switch flag
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+ 
+  if (switchByte == 1) {
+    Serial.println("SWITCHED");
+    flag = switchByte;
+    return;
+  }
+
+  String incoming = "";
+ 
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  // check length for error
+  if (incomingLength != incoming.length()) {   
+   Serial.println("error: message length does not match length");
+   return;                             
+  }
+ 
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             
+  }
+  
+  if (flag == 0) {
+    Serial.println("zero");
+  } else {
+    Serial.println("SWITCH");
+  }
+
+  if (incoming.equals("ON")){
+    digitalWrite(ledPin, HIGH);
+  }
+  if (incoming.equals("OFF")){
+    digitalWrite(ledPin, LOW);
+  }
+}
