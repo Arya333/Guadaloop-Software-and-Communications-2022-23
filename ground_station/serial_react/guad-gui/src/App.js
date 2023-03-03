773658f75c293b6bb80a129b3b1276281f7683b6
Christos <christosjoseph11@gmail.com>
2023-03-03 11:54:12 -0600

    added led to turn on when arduino button is pressed,

--- ground_station/serial_react/guad-gui/src/App.js
+++ ground_station/serial_react/guad-gui/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
- console.log("dom fire")
  const[port, setPort] =  useState();
-
  const [bit_val, setBit_val] = useState(49);
  const [readval, setReadVal] = useState("");
+ const[led_style, setLedStyle] = useState("led-off");
+
 
   const openSerialPort = async () =>{
    
@@ -20,20 +20,17 @@ function App() {
   }
 
   const flash = async() => {
+    
    let writer = port.writable.getWriter()
     
     // if (writer['desiredSize'] != undefined){
     //   console.log("writer is: "+ writer['desiredSize'])
     const data = new Uint8Array([49]); // hello
     await writer.write(data);
-
-    // if ( bit_val == 48){
-    //   setBit_val(49)
-    // }else {
-    //   setBit_val(48)
-    // }
+    
     // Allow the serial port to be closed later.
     writer.releaseLock();
+    console.log("writing 1");
   }
   
 const off = async()=>{
@@ -41,12 +38,12 @@ const off = async()=>{
 
   const data = new Uint8Array([48]); // hello
   await writer.write(data);
+  console.log("writing 0");
+ 
   // Allow the serial port to be closed later.
   writer.releaseLock();
 }
-function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
+
 const read =async() =>{
 // Listen to data coming from the serial device.
 const textDecoder = new TextDecoder();
@@ -61,20 +58,20 @@ while (true) {
   }
   // value is a Uint8Array.
  
- 
-    setBit_val(textDecoder.decode(value))
-    console.log(textDecoder.decode(value));
-
-    // await delay(1000);
+  if(textDecoder.decode(value) ==1){
+    setLedStyle("led-blue")
+    }else if(textDecoder.decode(value) ==0){
+      setLedStyle("led-off")
+    }
+    setReadVal(textDecoder.decode(value));
+    //console.log(textDecoder.decode(value));
 }
 }
 const toggle_light = async() => {
   let writer = port.writable.getWriter()
   
-  
   const data = new Uint8Array([bit_val]); // hello
   await writer.write(data);
-
  
   // Allow the serial port to be closed later.
   writer.releaseLock();
@@ -93,8 +90,11 @@ const toggle_light = async() => {
     <button onClick={()=> read()}> read</button>
     <button onClick={()=> toggle_light()}> light toggle</button>
     <h1>{bit_val}</h1>
-
-    <h2>{readval}</h2>
+    <div className="led-box">
+    <div className={led_style}></div>
+    <p>Blue LED</p>
+  </div>
+   
     </div>
   );
 }
