4c862df2132a5f1c923fa373b391d817a443391a
RaviPatel021 <92270576+RaviPatel021@users.noreply.github.com>
2023-03-23 17:48:22 -0500

    Update Nano_twoway_final.ino

--- Arduino IDE/Nano_twoway_final/Nano_twoway_final.ino
+++ Arduino IDE/Nano_twoway_final/Nano_twoway_final.ino
@@ -10,12 +10,14 @@ void setup() {
   pinMode(buttonPin, INPUT);
 
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("LoRa Sender");
-  
+
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
 
   LoRa.setTxPower(20);
@@ -24,64 +26,30 @@ void setup() {
 int starttime;
 int endtime;
 void loop() {
-  starttime = millis();
-  endtime = starttime;
-  while ((endtime - starttime) <=100) // do this loop for up to 1000mS
-  {
-    receive();
-    endtime = millis();
-  }
-  send();
-
-
-
-
-  // delay(5000);
-
+  receive();
+  // delay(50);
 }
 
-void send(){
-  // Serial.print("Sending packet: ");
-  // Serial.println(counter);
-
-  // send packet
-  if(digitalRead(buttonPin)){
-    LoRa.beginPacket();
-    LoRa.print(1);
-    // LoRa.print(counter);
-    LoRa.endPacket();
-  }
-  else{
-    LoRa.beginPacket();
-    LoRa.print(0);
-    // LoRa.print(counter);
-    LoRa.endPacket();
-  }
-
+void send(String s) {
+  LoRa.beginPacket();
+  LoRa.print(s);
+  LoRa.endPacket();
+  Serial.println("Sending packet: " + s);
 }
 
-char c;
-void receive(){
+String message = "";
+
+void receive() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
-    // read packet
-    while (LoRa.available()) {
-      c = LoRa.read();
-      if(c=='1'){
-        digitalWrite(ledPin, HIGH);
-
-      }
-      else if (c=='0'){
-        digitalWrite(ledPin, LOW);
-      }
-      Serial.print(c);
+    message = LoRa.readString();
+    Serial.println(message);
+    delay(10);
+    if (message == "SEND") {
+      send(String(counter));
+      counter = counter + 1;
+    } else {
+      send(message);
     }
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-
   }
 }
