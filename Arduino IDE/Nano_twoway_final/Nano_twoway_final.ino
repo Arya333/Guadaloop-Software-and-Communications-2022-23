6c9a0862aaec930d09036b4b264c9ae367770de9
RaviPatel021 <92270576+RaviPatel021@users.noreply.github.com>
2023-03-29 18:27:31 -0500

    Update Nano_twoway_final.ino

--- Arduino IDE/Nano_twoway_final/Nano_twoway_final.ino
+++ Arduino IDE/Nano_twoway_final/Nano_twoway_final.ino
@@ -2,6 +2,7 @@
 #include <LoRa.h>
 const int ledPin = 7;
 const int buttonPin = 8;
+int buttonState = 0;
 
 int counter = 0;
 
@@ -9,13 +10,13 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial)
     ;
-  Serial.println("LoRa Sender");
+  // Serial.println("LoRa Sender");
 
   if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
+    // Serial.println("Starting LoRa failed!");
     while (1)
       ;
   }
@@ -34,22 +35,44 @@ void send(String s) {
   LoRa.beginPacket();
   LoRa.print(s);
   LoRa.endPacket();
-  Serial.println("Sending packet: " + s);
+  // Serial.println("Sending packet: " + s);
 }
 
 String message = "";
+String c = "";
 
 void receive() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     message = LoRa.readString();
-    Serial.println(message);
     delay(10);
     if (message == "SEND") {
-      send(String(counter));
-      counter = counter + 1;
+      if(Serial.available()){
+        c = Serial.readString();
+        if(c.substring(0,1).equals("1")){
+          send("1");
+        }
+        else if(c.substring(0,1).equals("0")){
+          send("0");
+        }
+        else{
+          send("2");
+        }
+      }
+      else{
+        send("2");
+      }
+      // buttonState = digitalRead(buttonPin);
+      // if (buttonState == HIGH) {
+      //   send("1");             
+      // } else {
+      //   send("0");
+      // }
+      // send(String(counter));
+      // counter = counter + 1;
     } else {
-      send(message);
+      Serial.println(message.substring(0));
+      send(message.substring(0,3));
     }
   }
 }
